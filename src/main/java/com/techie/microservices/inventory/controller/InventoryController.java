25b8904f5b07813abe5561532a935906f450aa7c
LukaOniani <lukaa.oniani2002@gmail.com>
2025-07-15 15:56:49 +0400

    add InventoryController for inventory stock checking

---
+++ src/main/java/com/techie/microservices/inventory/controller/InventoryController.java
@@ -0,0 +1,25 @@
+package com.techie.microservices.inventory.controller;
+
+import com.techie.microservices.inventory.repository.InventoryRepository;
+import com.techie.microservices.inventory.service.InventoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/inventory")
+public class InventoryController {
+  private final InventoryService inventoryService;
+
+  @GetMapping
+  @ResponseStatus(HttpStatus.OK)
+  public boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity) {
+    return inventoryService.isInStock(skuCode, quantity);
+  }
+
+}
